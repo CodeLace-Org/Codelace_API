24278a05f95a9d7059b615b3ed37b76a6f83b570
Andre <andrepilcoch@gmail.com>
2024-04-24 10:53:29 -0500

    feat: estructura inicial de CodeLace API

---
+++ codelace/src/main/java/com/codelace/codelace/CodelaceApplication.java
@@ -0,0 +1,13 @@
+package com.codelace.codelace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelaceApplication.class, args);
+	}
+
+}
---
+++ codelace/src/test/java/com/codelace/codelace/CodelaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelace.codelace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
