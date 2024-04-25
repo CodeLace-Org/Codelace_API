35d70dd70f0efd32dbb3baf50ccc419e7ec751cb
LuisFelipePoma <u202110902@upc.edu.pe>
2024-04-24 21:25:13 -0500

    feature: Se completo el archivo mapper de rutas

--- codelace/src/main/java/com/codelace/codelace/mapper/RutaMapper.java
+++ codelace/src/main/java/com/codelace/codelace/mapper/RutaMapper.java
@@ -1,5 +1,32 @@
 package com.codelace.codelace.mapper;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.codelace.codelace.model.dto.RutaRequestDTO;
+import com.codelace.codelace.model.dto.RutaResponseDTO;
+import com.codelace.codelace.model.entity.Ruta;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
 public class RutaMapper {
-	
+	private final ModelMapper modelMapper;
+
+	public Ruta convertToEntity(RutaRequestDTO rutaRequestDTO) {
+		return modelMapper.map(rutaRequestDTO, Ruta.class);
+	}
+
+	public RutaResponseDTO convertToDTO(Ruta ruta) {
+		return modelMapper.map(ruta, RutaResponseDTO.class);
+	}
+
+	public List<RutaResponseDTO> convertToListDTO(List<Ruta> rutas) {
+		return rutas.stream()
+				.map(this::convertToDTO)
+				.toList();
+	}
 }
