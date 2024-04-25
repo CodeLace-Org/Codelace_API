0afa71b240830b4cfea284e433b7b489800c8222
LuisFelipePoma <u202110902@upc.edu.pe>
2024-04-24 21:17:24 -0500

    feature: Se completo el archivo repository de rutas

--- codelace/src/main/java/com/codelace/codelace/repository/RutaRepository.java
+++ codelace/src/main/java/com/codelace/codelace/repository/RutaRepository.java
@@ -1,5 +1,11 @@
 package com.codelace.codelace.repository;
 
-public interface RutaRepository {
-	
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codelace.codelace.model.entity.Ruta;
+
+public interface RutaRepository extends JpaRepository<Ruta, Long> {
+	Optional<Ruta> findById(Optional<Long> id);
 }
