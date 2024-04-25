f8f2f8322947d175cc979c4a9f6b33b20ff2b393
Andre <andrepilcoch@gmail.com>
2024-04-24 22:40:34 -0500

    feat: repository de estudiante

--- codelace/src/main/java/com/codelace/codelace/repository/EstudianteRepository.java
+++ codelace/src/main/java/com/codelace/codelace/repository/EstudianteRepository.java
@@ -1,5 +1,13 @@
 package com.codelace.codelace.repository;
 
-public interface EstudianteRepository {
-	
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codelace.codelace.model.entity.Estudiante;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long>{
+	Optional<Estudiante> findById(Optional<Long> id);
+	Optional<Estudiante> findByUsuario(Optional<Long> usuario);
+    Optional<Estudiante> findByEmail(Optional<String> email);
 }
