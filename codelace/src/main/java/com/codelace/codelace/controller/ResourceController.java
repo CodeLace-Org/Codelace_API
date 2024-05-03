adfe989e671c268b58dd7522da3ae0ee57ed4079
Renato <renatoalcalde285@gmail.com>
2024-05-02 21:54:30 -0500

    feature: Se implementó un GET de recursos por su ID

--- codelace/src/main/java/com/codelace/codelace/controller/ResourceController.java
+++ codelace/src/main/java/com/codelace/codelace/controller/ResourceController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -27,11 +28,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ResourceController {
 	private final ResourceService resourceService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ResourceResponseDTO> getResourceById(@PathVariable Long id){
+        ResourceResponseDTO resource = resourceService.getResourceById(id);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
     @GetMapping
     public ResponseEntity<List<ResourceResponseDTO>> getAllResources(){
         List<ResourceResponseDTO> resources = resourceService.getAllResource();
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
+
 
     @PostMapping
     public ResponseEntity<ResourceResponseDTO> createResource(@Validated @RequestBody ResourceRequestDTO resourceRequestDTO){
