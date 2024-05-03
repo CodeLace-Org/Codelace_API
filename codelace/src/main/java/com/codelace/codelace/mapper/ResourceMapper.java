cbd343264a66cdb1678070a113a92d0ee466e60e
JohnSovero <u202115065@upc.edu.pe>
2024-05-02 21:27:03 -0500

    fix: cambio de nombre de ResourceRespond a ResourceResponse

--- codelace/src/main/java/com/codelace/codelace/mapper/ResourceMapper.java
+++ codelace/src/main/java/com/codelace/codelace/mapper/ResourceMapper.java
@@ -6,7 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.codelace.codelace.model.dto.ResourceRequestDTO;
-import com.codelace.codelace.model.dto.ResourceRespondDTO;
+import com.codelace.codelace.model.dto.ResourceResponseDTO;
 import com.codelace.codelace.model.entity.Resource;
 
 import lombok.AllArgsConstructor;
@@ -20,11 +20,11 @@ public class ResourceMapper {
         return modelMapper.map(resourceRequestDTO, Resource.class);
     }
 
-    public ResourceRespondDTO convertToDTO(Resource resource){
-        return modelMapper.map(resource, ResourceRespondDTO.class);
+    public ResourceResponseDTO convertToDTO(Resource resource){
+        return modelMapper.map(resource, ResourceResponseDTO.class);
     }
 
-    public List<ResourceRespondDTO> convertToListDTO(List<Resource> resource) {
+    public List<ResourceResponseDTO> convertToListDTO(List<Resource> resource) {
         return resource.stream()
                 .map(this::convertToDTO)
                 .toList();
--- codelace/src/main/java/com/codelace/codelace/service/ResourceService.java
+++ codelace/src/main/java/com/codelace/codelace/service/ResourceService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import com.codelace.codelace.exception.ResourceNotFoundException;
 import com.codelace.codelace.mapper.ResourceMapper;
 import com.codelace.codelace.model.dto.ResourceRequestDTO;
-import com.codelace.codelace.model.dto.ResourceRespondDTO;
+import com.codelace.codelace.model.dto.ResourceResponseDTO;
 import com.codelace.codelace.model.entity.Project;
 import com.codelace.codelace.model.entity.Resource;
 import com.codelace.codelace.repository.ProjectRepository;
@@ -22,17 +22,17 @@ public class ResourceService {
     private final ResourceRepository resourceRepository;
     private final ProjectRepository projectRepository;
 
-    public List<ResourceRespondDTO> getAllResource(){
+    public List<ResourceResponseDTO> getAllResource(){
         List<Resource> resources = resourceRepository.findAll();
         return resourceMapper.convertToListDTO(resources);
     }
 
-    public ResourceRespondDTO getResourceById(Long id){
+    public ResourceResponseDTO getResourceById(Long id){
         Resource resource = resourceRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Resource not fount"));
         return resourceMapper.convertToDTO(resource);
     }
 
-    public ResourceRespondDTO createResource(ResourceRequestDTO resourceRequestDto) {
+    public ResourceResponseDTO createResource(ResourceRequestDTO resourceRequestDto) {
         Resource resource = resourceMapper.convertToEntity(resourceRequestDto);
         Project project = projectRepository.findById(resourceRequestDto.getProject()).orElseThrow(()->new ResourceNotFoundException("Not found"));
         resource.setProject(project);
