a90217531c482c137e727a18866dac2b316438dc
LuisFelipePoma <u202110902@upc.edu.pe>
2024-05-19 19:07:44 -0500

    feature: Se implemento la visualizacion de post por estudiante

--- codelace/src/main/java/com/codelace/codelace/repository/PostRepository.java
+++ codelace/src/main/java/com/codelace/codelace/repository/PostRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codelace.codelace.model.entity.Post;
 import com.codelace.codelace.model.entity.Project;
+import com.codelace.codelace.model.entity.Student;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
 	Optional<List<Post>> findAllByProject(Project project);
+	Optional<List<Post>> findAllByStudent(Student student);
 }
--- codelace/src/main/java/com/codelace/codelace/service/PostService.java
+++ codelace/src/main/java/com/codelace/codelace/service/PostService.java
@@ -27,64 +27,78 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class PostService {
-    private PostRepository postRepository;
-    private ProjectRepository projectRepository;
-    private StudentRepository studentRepository;
-    private RocketRepository rocketRepository;
-    private CommentRepository commentRepository;
-    private PostMapper postMapper;
+	private PostRepository postRepository;
+	private ProjectRepository projectRepository;
+	private StudentRepository studentRepository;
+	private RocketRepository rocketRepository;
+	private CommentRepository commentRepository;
+	private PostMapper postMapper;
 
-    // Method that returns all the posts
-    public List<PostResponseDTO> getAllPosts(){
-        List<Post> posts = postRepository.findAll();
-        return postMapper.convertToListDTO(posts);
-    }
+	// Method that returns all the posts
+	public List<PostResponseDTO> getAllPosts() {
+		List<Post> posts = postRepository.findAll();
+		return postMapper.convertToListDTO(posts);
+	}
 
-    //Method that creates a post
-    public PostResponseDTO createPost(PostRequestDTO postRequestDTO){
-        Post post = postMapper.convertToEntity(postRequestDTO);
-        Project project = projectRepository.findById(postRequestDTO.getProject())
-            .orElseThrow(()->new ResourceNotFoundException("Project not found with id " + postRequestDTO.getProject()));
-        Student student = studentRepository.findById(postRequestDTO.getStudent())
-            .orElseThrow(()->new ResourceNotFoundException("Student not found with id " + postRequestDTO.getStudent()));
+	// Method that creates a post
+	public PostResponseDTO createPost(PostRequestDTO postRequestDTO) {
+		Post post = postMapper.convertToEntity(postRequestDTO);
+		Project project = projectRepository.findById(postRequestDTO.getProject())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"Project not found with id " + postRequestDTO.getProject()));
+		Student student = studentRepository.findById(postRequestDTO.getStudent())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"Student not found with id " + postRequestDTO.getStudent()));
 
-        post.setProject(project);
-        post.setStudent(student);
-        post.setDate(LocalDate.now());
-        postRepository.save(post);
-        return postMapper.convertToDTO(post);
-    }
-    
-    // Method that returns all the posts by project id
-	public List<PostByProjectResponseDTO> getPostsByProjectId(Long id){
-        Project project = projectRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Project not found with id " + id));
-        List<Post> posts = postRepository.findAllByProject(project)
-            .orElseThrow(() -> new ResourceNotFoundException("Posts not found for project with id " + id));
-        
-        List<PostByProjectResponseDTO> postByProjectResponseDTOList = new ArrayList<>();
-        Long aux = (long) 1;
-        if(!posts.isEmpty()){
-            Student student = studentRepository.findById(posts.get(0).getStudent().getId())
-                .orElseThrow(() -> new ResourceNotFoundException("Student not found with id " + posts.get(0).getStudent().getId()));
+		post.setProject(project);
+		post.setStudent(student);
+		post.setDate(LocalDate.now());
+		postRepository.save(post);
+		return postMapper.convertToDTO(post);
+	}
 
-            for (Post post : posts) {
-                PostByProjectResponseDTO postByProjectResponseDTO = new PostByProjectResponseDTO();
-                List<Rocket> rockets = rocketRepository.findAllByPostAndStudent(post, student)
-                        .orElseThrow(() -> new ResourceNotFoundException("Rockets not found for post with id " + id));
-                List<Comment> comments = commentRepository.findAllByPostAndStudent(post, student)
-                        .orElseThrow(() -> new ResourceNotFoundException("Comments not found for post with id " + id));
+	// Method that returns all the posts by project id
+	public List<PostByProjectResponseDTO> getPostsByProjectId(Long id) {
+		Project project = projectRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Project not found with id " + id));
+		List<Post> posts = postRepository.findAllByProject(project)
+				.orElseThrow(() -> new ResourceNotFoundException("Posts not found for project with id " + id));
 
-                postByProjectResponseDTO.setId(aux);
-                postByProjectResponseDTO.setStudent(student);
-                postByProjectResponseDTO.setRockets((long) rockets.size());
-                postByProjectResponseDTO.setDate(LocalDate.now());
-                postByProjectResponseDTO.setImage(post.getImage());
-                postByProjectResponseDTO.setComments((long) comments.size());
-                postByProjectResponseDTOList.add(postByProjectResponseDTO);
-                aux += 1;
-            }
-        }   
-        return postByProjectResponseDTOList;
-    }
+		List<PostByProjectResponseDTO> postByProjectResponseDTOList = new ArrayList<>();
+		Long aux = (long) 1;
+		if (!posts.isEmpty()) {
+			Student student = studentRepository.findById(posts.get(0).getStudent().getId())
+					.orElseThrow(() -> new ResourceNotFoundException(
+							"Student not found with id " + posts.get(0).getStudent().getId()));
+
+			for (Post post : posts) {
+				PostByProjectResponseDTO postByProjectResponseDTO = new PostByProjectResponseDTO();
+				List<Rocket> rockets = rocketRepository.findAllByPostAndStudent(post, student)
+						.orElseThrow(() -> new ResourceNotFoundException("Rockets not found for post with id " + id));
+				List<Comment> comments = commentRepository.findAllByPostAndStudent(post, student)
+						.orElseThrow(() -> new ResourceNotFoundException("Comments not found for post with id " + id));
+
+				postByProjectResponseDTO.setId(aux);
+				postByProjectResponseDTO.setStudent(student);
+				postByProjectResponseDTO.setRockets((long) rockets.size());
+				postByProjectResponseDTO.setDate(LocalDate.now());
+				postByProjectResponseDTO.setImage(post.getImage());
+				postByProjectResponseDTO.setComments((long) comments.size());
+				postByProjectResponseDTOList.add(postByProjectResponseDTO);
+				aux += 1;
+			}
+		}
+		return postByProjectResponseDTOList;
+	}
+
+	// Method that returns a list of post by student id
+	public List<PostResponseDTO> getPostsByStudent(Long id_student) {
+		Student student = studentRepository
+				.findById(id_student)
+				.orElseThrow(() -> new ResourceNotFoundException("Student not found with id " + id_student));
+		List<Post> posts = postRepository
+				.findAllByStudent(student)
+				.orElseThrow(() -> new ResourceNotFoundException("Posts not found for student with id " + id_student));
+		return postMapper.convertToListDTO(posts);
+	}
 }
