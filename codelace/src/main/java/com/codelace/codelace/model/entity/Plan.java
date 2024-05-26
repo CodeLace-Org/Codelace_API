79c02aed096b5fcba634596f80040d8290f9bca9
Andre <andrepilcoch@gmail.com>
2024-05-26 16:34:16 -0500

    feat: service, controller y PlanResponseDTO para la obtención de planes

--- codelace/src/main/java/com/codelace/codelace/model/entity/Plan.java
+++ codelace/src/main/java/com/codelace/codelace/model/entity/Plan.java
@@ -1,5 +1,7 @@
 package com.codelace.codelace.model.entity;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,7 +26,7 @@ public class Plan {
     private String type;
 
     @Column(name = "price", nullable = false)
-    private Double price;
+    private BigDecimal price;
 
     @Column(name = "description", nullable = false)
     private String description;
