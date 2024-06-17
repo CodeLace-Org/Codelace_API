e466569a6ab08fd1af772e7e41b0f77ecedd4aee
Andre <andrepilcoch@gmail.com>
2024-06-16 23:51:02 -0500

    fix: student Response email addition

--- codelace/src/main/java/com/codelace/codelace/model/dto/StudentResponseDTO.java
+++ codelace/src/main/java/com/codelace/codelace/model/dto/StudentResponseDTO.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class StudentResponseDTO {
     private Long id;
+    private String email;
     private String username;
     private String status;
     private String description;
