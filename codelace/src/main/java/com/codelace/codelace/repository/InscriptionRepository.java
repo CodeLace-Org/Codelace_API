ddab1320f62e167cdd7d4cbcbf0660c10e20ccfd
Andre <andrepilcoch@gmail.com>
2024-06-21 17:15:22 -0500

    feat: obtener las inscripciones de un usuario

--- codelace/src/main/java/com/codelace/codelace/repository/InscriptionRepository.java
+++ codelace/src/main/java/com/codelace/codelace/repository/InscriptionRepository.java
@@ -1,5 +1,6 @@
 package com.codelace.codelace.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +14,6 @@ public interface InscriptionRepository extends JpaRepository<Inscription, Long>{
     Optional<Inscription> findById(Optional<Long> id);
     Optional<Inscription> findByRoute(Route route);
     Optional<Inscription> findByStudent(Student student);
+    List<Inscription> findAllByStudent(Student student);
     Optional<Inscription> findByStudentAndRoute(Student student, Route route);
 }
