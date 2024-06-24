54be288df5526a22a5b4680bbc521278e27d303e
LuisFelipePoma <u202110902@upc.edu.pe>
2024-06-24 12:10:34 -0500

    sysntax error

--- codelace/src/main/java/com/codelace/codelace/service/ResourceService.java
+++ codelace/src/main/java/com/codelace/codelace/service/ResourceService.java
@@ -3,7 +3,6 @@ package com.codelace.codelace.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
 import com.codelace.codelace.exception.ResourceDuplicateException;
 import com.codelace.codelace.exception.ResourceNotFoundException;
 import com.codelace.codelace.mapper.ResourceMapper;
@@ -11,7 +10,6 @@ import com.codelace.codelace.model.dto.ResourceRequestDTO;
 import com.codelace.codelace.model.dto.ResourceResponseDTO;
 import com.codelace.codelace.model.entity.Project;
 import com.codelace.codelace.model.entity.Resource;
-import com.codelace.codelace.repository.InscriptionRepository;
 import com.codelace.codelace.repository.ProjectRepository;
 import com.codelace.codelace.repository.ResourceRepository;
 
@@ -23,7 +21,6 @@ public class ResourceService {
 	private final ResourceMapper resourceMapper;
     private final ResourceRepository resourceRepository;
     private final ProjectRepository projectRepository;
-    private final InscriptionRepository inscriptionrepository;
 
     public List<ResourceResponseDTO> getAllResource(){
         List<Resource> resources = resourceRepository.findAll();
@@ -37,7 +34,6 @@ public class ResourceService {
 
     public List<ResourceResponseDTO> getAllByProject(Long id){
         Project project = projectRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Project not found"));
-        inscriptionrepository.findByRoute(project.getRoute()).orElseThrow(()-> new ResourceNotFoundException("Inscription not found"));
         List<Resource> resources = resourceRepository.findAllByProject(project);
         return resourceMapper.convertToListDTO(resources); 
     }
